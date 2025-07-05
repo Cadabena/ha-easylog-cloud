b186c88271076b8ffa326026d6583a1122fbf220
Ben Walters <cadabena@gmail.com>
2025-07-05 23:49:19 +0100

    Added extra domains for pytest

--- custom_components/ha_easylog_cloud/const.py
+++ custom_components/ha_easylog_cloud/const.py
@@ -3,3 +3,7 @@ CONF_USERNAME = "username"
 CONF_PASSWORD = "password"
 PLATFORMS = ["sensor"]
 STARTUP_MESSAGE = "Starting EasyLog Cloud integration"
+BINARY_SENSOR = "binary_sensor"
+SENSOR = "sensor"
+SWITCH = "switch"
+DEFAULT_NAME = "easylog_cloud"
